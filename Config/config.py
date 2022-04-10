a18686190c69643423a7016419f0ac95226f4d37
S1GHE <94549535+S1GHE@users.noreply.github.com>
2022-04-10 12:29:44 +0000

    Add Config/

---
+++ Config/config.py
@@ -0,0 +1,26 @@
+# Ссылки на расписание
+url_090301_even_week = 'https://docs.google.com/document/d/e/2PACX-1vSdbwO-ZFRJLw6xjUbEAy4Kyy-FV2ueZwFF4sTHD' \
+                       '-VLsSoEjUi6yXC1QVFwZKkMAbveNegvzckVL4tv/pub '
+url_090301_odd_week = 'https://docs.google.com/document/d/e/2PACX-1vQzEFeSyHCSt7NqXBcOc3JYlTc' \
+                      '-9xFdEk6h3jaSQugm2Ovz9lPFzvTGW3b2ASsJ1wwsiWDmkeFE6eVB/pub '
+url_090903_even_week = 'https://docs.google.com/document/pub?id=1EVpgp58G_bwdi6J5gNN_cjekG8loU4XHUw0yEJJaf1E'
+
+url_090903_odd_week = 'https://docs.google.com/document/pub?id=1ZjBfEvJzmluiZy-5HqvulPHHfqjYTbxltDp4hbCdZWc'
+
+url_150305_even_week = 'https://docs.google.com/document/pub?id=1XhFa8KhZGxFa6Vk7ynqNrJCioT4V6hBq5OeewejY2J4'
+
+url_150305_odd_week = 'https://docs.google.com/document/pub?id=11VHl98tppDcTKpnU_WWooOXI69FUQ0O5YRxXFjQcThs'
+
+url_200301_odd_week = 'https://docs.google.com/document/pub?id=1CSiXhbqn3YE38LCWpxr2JRMXdtKmVTW6UqvNoxNbmHk'
+
+url_200301_even_week = 'https://docs.google.com/document/pub?id=1PYjZhWRvU8A5FAToKexYLxnL_vle9mHXpCrZek43r8Y'
+
+url_380301_odd_week = 'https://docs.google.com/document/pub?id=1UJeWi0oz4H9SSyD4CB8ztIs4i39L6QvxJO4q7x6mvxE'
+
+url_380301_even_week = 'https://docs.google.com/document/pub?id=1_-7gYxCvTjzXBeTeE9J155AKHEjnbbmxPRjFNE1l9cc'
+
+# Токен бота: '5246071663:AAEGSGRAxm4PD95YORU08iMjXZDCzdVpMNA'
+# Токен бота для тестирования: '5019443966:AAFwaVKsv8sSzs85J0rtICuywAFno0EGzZY'
+bot_token = "5019443966:AAFwaVKsv8sSzs85J0rtICuywAFno0EGzZY"
+
+admin_id = 477868775
